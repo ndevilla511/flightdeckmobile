73aed9e000eb4a21bcf6aaaa013e4733b5ea0b90
ndevilla511 <ndevilla511@gmail.com>
2018-05-18 17:29:16 -0700

    added react-navigation, setting up router file

---
+++ src/config/router.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { StackNavigator, TabNavigator } from 'react-navigation';
+import LogIn from '../screens/LogIn/LogIn';
+import OrderSearch from '../screens/OrderSearch/OrderSearch';
+import OrderDetails from '../screens/OrderDetails/OrderDetails';
